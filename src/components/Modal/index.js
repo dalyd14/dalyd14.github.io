8d9fa92dd6dd9206722961d95aa578ae12127533
dalyd14 <dalyd14@gmail.com>
2021-03-20 14:06:04 -0400

    got the icons to automatically be rendered via string in project object; issue with tic tac whoa

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,9 +1,31 @@
 import React from 'react'
 import './modal.css'
 
-import { Github, Mongodb, Javascript, CssThree, Html5, NodeDotJs, Express, SocketDotIo, Bootstrap } from '@icons-pack/react-simple-icons'
+import { Github, Mongodb, Mysql, Javascript, CssThree, 
+    Html5, NodeDotJs, Express, Jquery, SocketDotIo, Bootstrap } from '@icons-pack/react-simple-icons'
+
+const Technologies = {
+    "Html5": Html5,
+    "CssThree": CssThree,
+    "Javascript": Javascript,
+    "Jquery": Jquery,
+    "Bootstrap": Bootstrap,
+    "NodeDotJs": NodeDotJs,
+    "Express": Express,
+    "Mysql": Mysql,
+    "Mongodb": Mongodb,
+    "SocketDotIo": SocketDotIo
+}
 
 const Modal = ({ onClose, currentProject }) => {
+
+    // const techComponents = currentProject.technologies.map((tech, i) => {
+    //     const TechComp = Technologies[tech];
+    //     return <TechComp key={i} />
+    // })
+
+    // console.log(techComponents)
+
 
     return (
         <div className="modalBackdrop">
@@ -13,6 +35,25 @@ const Modal = ({ onClose, currentProject }) => {
                 <button type="button" className="close" onClick={onClose} aria-label="Close">
                     <span className="material-icons">clear</span>
                 </button>
+                <div className="technologies-icons">
+                    {
+                        currentProject.technologies.map((tech, i) => {
+                            if (tech in Technologies) {
+                                const TechComp = Technologies[tech];
+                                console.log(TechComp)
+                                return <TechComp key={i} />                                
+                            } else if (tech === "Server side APIs") {
+                                return (
+                                    <span key={i} className="material-icons">dns</span>
+                                )
+                            } else {
+                                return (
+                                    <span key={i} className="material-icons">construction</span>
+                                )
+                            }
+                        })
+                    }
+                </div>
                 <div className="project-btn-group">
                     <a type="button" href={currentProject.site_url} target="_blank" className="project-btn btn btn-light">
                         <span className="material-icons">language</span>
@@ -23,7 +64,9 @@ const Modal = ({ onClose, currentProject }) => {
                         Visit GitHub
                     </a>                    
                 </div>
-
+                <div className="project-description">
+                    {currentProject.description}
+                </div>
             </div>
         </div>
 
